c27c36a688427016fe2cf3e7220db5601e9ff5d4
MAsadAnwaar <asadattal58@gmail.com>
2024-08-28 10:18:38 +0500

    gap remove

--- games/settings.py
+++ games/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 import os
+from .jazzmin import JAZZMIN_SETTINGS
 # from .jazzmin import JAZZMIN_SETTINGS
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +134,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # JAZZMIN_SETTINGS = JAZZMIN_SETTINGS
+JAZZMIN_SETTINGS = JAZZMIN_SETTINGS
--- gamesite/admin.py
+++ gamesite/admin.py
@@ -1,15 +1,29 @@
-# admin.py
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Card, BackgroundImage
 
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image')  # Fields to display in the list view
+    list_display = ('name', 'image_preview')  # Updated to use image_preview method
     search_fields = ('name',)  # Fields to search within
     list_filter = ('name',)  # Fields to filter by
 
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="width: 100px; height: auto;" />', obj.image.url)
+        return 'No Image'
+    
+    image_preview.short_description = 'Image Preview'
+
 @admin.register(BackgroundImage)
 class BackgroundImageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image')  # Fields to display in the list view
+    list_display = ('name', 'image_preview')  # Updated to use image_preview method
     search_fields = ('name',)  # Fields to search within
     list_filter = ('name',)  # Fields to filter by
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="width: 100px; height: auto;" />', obj.image.url)
+        return 'No Image'
+    
+    image_preview.short_description = 'Image Preview'
